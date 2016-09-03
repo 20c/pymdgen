270f36ae9a71f39d95792ae40f841c4ae6e865dd
Matt Griswold <grizz@20c.com>
2016-09-03 06:40:09 +0000

    add logging

--- pymdgen.py
+++ pymdgen.py
@@ -4,7 +4,11 @@ from __future__ import print_function
 import click
 import importlib
 import inspect
+import logging
 from pprint import pprint
+
+
+log = logging.getLogger('pymdgen')
 
 
 def doc_func(name, func, section_level=4):
@@ -69,16 +73,22 @@ def doc_class(name, cls, section_level=3):
 
 
 @click.command()
+@click.option('--debug', help='display debug messages', is_flag=True, default=False)
 @click.option('--section-level', help='markdown section level', default=3)
 @click.argument('modules', nargs=-1)
-def main(modules, section_level):
+def main(modules, debug, section_level):
     """ inspects given python modules and prints markdown """
+
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+
     for name in modules:
         module = importlib.import_module(name)
 
         for k, v in inspect.getmembers(module):
             if k == '__builtins__':
                 continue
+            log.debug("checking %s:%s" % (v, k))
             if inspect.isfunction(v):
                 doc_func(k, v, section_level)
             if inspect.isclass(v):
